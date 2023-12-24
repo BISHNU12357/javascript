17e62098d8f1fc9b185074d74eff2c04710a83a9
BISHNU12357 <bishnugsahoo@gmail.com>
2023-12-24 10:12:27 +0530

    second commit

---
+++ 03basic.js
@@ -0,0 +1,10 @@
+let score = "33";
+
+console.log( typeof ( score ) );
+let valInNumber = Number( score );
+console.log( typeof ( valInNumber ) );
+
+// operations
+let value = 3;
+let negValue = -value;
+console.log( negValue );
---
+++ 04datatypes.js
@@ -0,0 +1,44 @@
+"use strict"
+
+// there are 2 types of datatypes in javascript based on how they access the memory
+
+//-------------------------primitive----------------------------------------------
+
+// Number
+let num = 121;
+
+//string
+let str = "Bishnu";
+
+//boolean
+let valid = true;
+
+//null 
+let value = null;
+
+//undefined
+let val = undefined;
+
+// Symbol
+let sym = Symbol('123');
+
+//big int 
+let num2 = 12345678956n;
+
+//--------------------------non-primitive(refrence)------------------------------------------
+
+//array
+let arr = ["bishnu", "nitesh", "srr", "amartya"];
+
+// object
+let obj = {
+    name: "bishnu",
+    age: 21,
+    college: "vssut"
+}
+
+// function
+const doit = () =>{
+    console.log("just do it");
+}
+
